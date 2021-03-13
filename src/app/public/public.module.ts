065348b28d9367e215ac5082ce763b5e4fe01145
FranklinParker <fparker@deliveryman.com>
2021-03-13 11:19:22 -0500

    sae

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PublicComponent } from './components/public/public.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PublicComponent],
   imports: [
     CommonModule
   ]
