05e518e438e8d03aecbda2d71686893440185a3f
FranklinParker <fparker@deliveryman.com>
2021-03-13 11:22:14 -0500

    save

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './components/public/public.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 
 @NgModule({
-  declarations: [PublicComponent],
+  declarations: [PublicComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule
   ]
