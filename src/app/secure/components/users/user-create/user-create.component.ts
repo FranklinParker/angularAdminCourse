3de64addfc02060492e34bdd998f878ca9e03a22
FranklinParker <fparker@deliveryman.com>
2021-03-15 17:29:24 -0400

    user form

--- src/app/secure/components/users/user-create/user-create.component.ts
+++ src/app/secure/components/users/user-create/user-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Role} from '../../../../interfaces/role';
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.scss']
 })
 export class UserCreateComponent implements OnInit {
-
-  constructor() { }
+  roles: Role[] = [];
+  form: FormGroup = this.fb.group({
+    email: ['', Validators.compose([
+      Validators.required, Validators.email])],
+    first_name: ['', Validators.compose([
+      Validators.required])],
+    last_name: ['', Validators.compose([
+      Validators.required])],
+    role_id: ['', Validators.compose([
+      Validators.required])],
+  }, {validators: []});
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  submit(): void{
+
+  }
 }
