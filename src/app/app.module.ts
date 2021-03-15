5e0f078d3545d0683bd205240a6f30a1a03826cd
FranklinParker <fparker@deliveryman.com>
2021-03-15 10:52:29 -0400

    interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SecureModule} from './secure/secure.module';
 import {PublicModule} from './public/public.module';
+import {CredentialsInterceptor} from './interceptors/Credentials.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,13 @@ import {PublicModule} from './public/public.module';
     SecureModule,
     PublicModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CredentialsInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
