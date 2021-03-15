6736156b5ecd04c1e193e56ae4a4d79368a24d00
FranklinParker <fparker@deliveryman.com>
2021-03-15 10:18:58 -0400

    save

--- src/app/secure/components/profile/profile.component.ts
+++ src/app/secure/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  infoForm: any;
+  passwordForm: any;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.infoForm = this.fb.group({
+      first_name: ['', Validators.compose([
+        Validators.required])],
+      last_name: ['', Validators.compose([
+        Validators.required])],
+      email: ['', Validators.compose([
+        Validators.required, Validators.email])],
+    }, {validators: []});
+    this.passwordForm = this.fb.group({
+      password: ['', Validators.compose([
+        Validators.required])],
+      password_confirm: ['', Validators.compose([
+        Validators.required])],
+    }, {validators: []});
   }
 
+  async infoSubmit(): Promise<void> {
+  }
+
+  async passwordSubmit(): Promise<void> {
+  }
 }
--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -6,6 +6,7 @@ import {MenuComponent} from './components/menu/menu.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 
@@ -19,7 +20,8 @@ import {RouterModule} from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
 
 })
