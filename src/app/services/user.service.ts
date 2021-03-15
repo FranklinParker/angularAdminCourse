582e518e838a448ead88fb2e423bc663a37e268a
FranklinParker <fparker@deliveryman.com>
2021-03-15 17:05:58 -0400

    save

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../interfaces/user';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {UserResponse} from '../interfaces/UserResponse';
@@ -9,7 +8,7 @@ import {UserResponse} from '../interfaces/UserResponse';
   providedIn: 'root'
 })
 export class UserService {
-  userApi = '${environment.api}/users';
+  userApi = `${environment.api}/users`;
   constructor(private http: HttpClient) { }
   async users(page: number): Promise<UserResponse>{
     const userResponse = await this.http.get<any>(`${this.userApi}?page=${page}`).toPromise();
