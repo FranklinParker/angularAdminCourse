b0dc3fe99bdf4956fa8356491c7e54a71817f845
FranklinParker <fparker@deliveryman.com>
2021-03-16 18:49:50 -0400

    save

--- src/app/interfaces/order.ts
+++ src/app/interfaces/order.ts
@@ -1,7 +1,11 @@
+import {OrderItem} from './orderItem';
+
 export interface Order{
   id: number;
   first_name: string;
   last_name: string;
   email: string;
   created_at: Date;
+  total: number;
+  orderItems: OrderItem[];
 }
