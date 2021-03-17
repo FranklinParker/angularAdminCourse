d321341a6b00dfc3232ac37852cc7c703a841cbc
FranklinParker <fparker@deliveryman.com>
2021-03-17 09:44:15 -0400

    clean imports

--- src/app/services/permission.service.ts
+++ src/app/services/permission.service.ts
@@ -1,8 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
-import {UserResponse} from '../interfaces/UserResponse';
 import {RestService} from './rest.service';
 
 @Injectable({
