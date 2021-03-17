72a1fbafcca988e04ba8ef43f2bbf3276054033b
FranklinParker <fparker@deliveryman.com>
2021-03-17 12:56:40 -0400

    got order export

--- src/app/secure/components/orders/orders.component.ts
+++ src/app/secure/components/orders/orders.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 
 import {OrderService} from '../../../services/order.service';
-import { Order } from '../../../interfaces/order';
+import {Order} from '../../../interfaces/order';
 
 @Component({
   selector: 'app-orders',
@@ -26,19 +26,21 @@ export class OrdersComponent implements OnInit {
   lastPage = 0;
   selectOrderId = -1;
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService) {
+  }
 
   async ngOnInit(): Promise<void> {
     await this.load();
   }
 
-  async load(page= 1): Promise<void>{
+  async load(page = 1): Promise<void> {
     const {data, meta} = await this.orderService.all(page);
     this.orders = data;
     this.lastPage = meta.last_page;
     console.log('orders', this.orders);
 
   }
+
   delete(id: number): void {
     if (confirm('Are you sure you want to delete this record?')) {
       this.orderService.delete(id).subscribe(
@@ -58,6 +60,15 @@ export class OrdersComponent implements OnInit {
   }
 
   export(): void {
-    alert('hi');
+    this.orderService.export().subscribe(
+      res => {
+        const blob = new Blob([res], {type: 'text/csv'});
+        const downloadUrl = window.URL.createObjectURL(res);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'orders.csv';
+        link.click();
+      }
+    );
   }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
@@ -8,9 +8,19 @@ import {RestService} from './rest.service';
 @Injectable({
   providedIn: 'root'
 })
-export class OrderService extends RestService{
+export class OrderService extends RestService {
   endpoint = `${environment.api}/orders`;
+
   constructor(protected http: HttpClient) {
     super(http);
+  }
+
+  export(): Observable<any> {
+    return this.http.post(`${environment.api}/export`, {}, {responseType: 'blob'});
+  }
+
+
+  chart(): Observable<any> {
+    return this.http.get(`${environment.api}/chart`);
   }
 }
