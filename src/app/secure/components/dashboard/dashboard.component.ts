4a53c585c9b58d46809939eeada769b3f8065136
FranklinParker <fparker@deliveryman.com>
2021-03-17 13:37:18 -0400

    charts

--- src/app/secure/components/dashboard/dashboard.component.ts
+++ src/app/secure/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {OrderService} from '../../../services/order.service';
+import * as c3 from 'c3';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+    const chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'x',
+        columns: [
+          ['x'],
+          ['Sales'],
+        ],
+        types: {
+          Sales: 'bar'
+        }
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    });
+
+    this.orderService.chart().subscribe(
+      (result: { date: string, sum: number }[]) => {
+        chart.load({
+          columns: [
+            ['x', ...result.map(r => r.date)],
+            ['Sales', ...result.map(r => r.sum)]
+          ]
+        });
+      }
+    );
   }
 
 }
